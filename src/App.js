7e7428ac04bf1a6a13b398b987c005d11d2e9910
Nadie Ivanitska <103319310+NadieIvi@users.noreply.github.com>
2022-10-17 13:02:45 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather app
-      <footer>
-        This project was coded by Nadie Ivanitska and is
-        <a
-          href="https://github.com/NadieIvi/react-weather-final"
-          target="_blank"
-        >
-          open-soursed on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        Weather app
+        <footer>
+          This project was coded by Nadie Ivanitska and is{" "}
+          <a
+            href="https://github.com/NadieIvi/react-weather-final"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-soursed on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
