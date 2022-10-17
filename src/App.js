5013bf52dd6b0f64c2784712b50086ce99e172b1
Nadie Ivanitska <103319310+NadieIvi@users.noreply.github.com>
2022-10-17 12:29:31 +0300

    Added GitHub repoUrl

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      Weather app
+      <footer>
+        This project was coded by Nadie Ivanitska and is
+        <a
+          href="https://github.com/NadieIvi/react-weather-final"
+          target="_blank"
+        >
+          open-soursed on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
